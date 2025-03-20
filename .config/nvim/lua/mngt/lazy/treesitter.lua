fe18c2ebac452796e9d46f7200d204b48a757134
mngt <80961204+mngste@users.noreply.github.com>
2025-03-20 09:29:05 +0100

    Update treesitter.lua

--- .config/nvim/lua/mngt/lazy/treesitter.lua
+++ .config/nvim/lua/mngt/lazy/treesitter.lua
@@ -6,7 +6,7 @@ return {
 
     configs.setup({
       ensure_installed = {
-        "c", "lua", "vim", "vimdoc", "elixir", "javascript", "html", "python", "typescript"
+        "c", "lua", "vim", "vimdoc", "elixir", "javascript", "html", "python", "typescript", "ninja", "rst"
       },
       sync_install = false,
       highlight = { enable = true },
